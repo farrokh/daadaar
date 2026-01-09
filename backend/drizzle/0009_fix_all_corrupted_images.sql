c9d7661e49486f7e96d9d5c4178af79a5ab07db9
farrokh <saytec@gmail.com>
2026-01-08 23:19:26 -0800

    fix(migration): incorrect column name in 0009

--- backend/drizzle/0009_fix_all_corrupted_images.sql
+++ backend/drizzle/0009_fix_all_corrupted_images.sql
@@ -8,7 +8,7 @@ UPDATE organizations
 SET logo_url = substring(logo_url from 'orgs/[^?]+')
 WHERE logo_url LIKE '%amazonaws.com/orgs/%';
 
--- Fix corrupted report images
-UPDATE reports
-SET image_url = substring(image_url from 'reports/[^?]+')
-WHERE image_url LIKE '%amazonaws.com/reports/%';
+-- Fix corrupted user profile images (if any)
+UPDATE users
+SET profile_image_url = substring(profile_image_url from 'users/[^?]+')
+WHERE profile_image_url LIKE '%amazonaws.com/users/%';
