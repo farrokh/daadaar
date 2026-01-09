d45218f65f2dc4ad9d22ec7ef90ef3c52060c5ea
farrokh <saytec@gmail.com>
2026-01-08 23:02:01 -0800

    fix(db): add data migration to clean up corrupted presigned URLs

---
+++ backend/drizzle/0008_fix_corrupted_images.sql
@@ -0,0 +1,14 @@
+-- Fix corrupted individuals profile images (remove expired presigned URLs)
+UPDATE individuals
+SET profile_image_url = substring(profile_image_url from 'users/[^?]+')
+WHERE profile_image_url LIKE '%amazonaws.com/users/%';
+
+-- Fix corrupted organization logos
+UPDATE organizations
+SET logo_url = substring(logo_url from 'orgs/[^?]+')
+WHERE logo_url LIKE '%amazonaws.com/orgs/%';
+
+-- Fix corrupted report images
+UPDATE reports
+SET image_url = substring(image_url from 'reports/[^?]+')
+WHERE image_url LIKE '%amazonaws.com/reports/%';
